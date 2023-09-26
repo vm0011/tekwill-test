9f527c1639861d99d37f371ba6db9dede3481222
Vadim Mirauta <test@test.test>
2023-09-27 00:59:38 +0300

    Reformat code.

--- src/main/java/homework_nr_8/TenWordDictionary.java
+++ src/main/java/homework_nr_8/TenWordDictionary.java
@@ -3,20 +3,20 @@ package homework_nr_8;
 import java.util.HashMap;
 
 public class TenWordDictionary {
-    HashMap<String,String> words = new HashMap<>();
+    HashMap<String, String> words = new HashMap<>();
 
     public void prepare() {
-        words.put("time","timp, oră");
-        words.put("is","este, a fi");
-        words.put("money","bani");
-        words.put("open","deschide");
-        words.put("mind","minte, gândire");
-        words.put("wide","larg");
-        words.put("for","pentru");
-        words.put("knowledge","cunoștințe");
+        words.put("time", "timp, oră");
+        words.put("is", "este, a fi");
+        words.put("money", "bani");
+        words.put("open", "deschide");
+        words.put("mind", "minte, gândire");
+        words.put("wide", "larg");
+        words.put("for", "pentru");
+        words.put("knowledge", "cunoștințe");
     }
 
-    public void translateWord(String word){
+    public void translateWord(String word) {
         prepare();
         System.out.println(word + " - (eng to ro): " + words.get(word));
     }
